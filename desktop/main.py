ff74177bfec1a284aa6dcac05c1f46fd0a683f42
sergnator <86975413+sergnator@users.noreply.github.com>
2024-03-22 17:08:20 +0300

    начало работа над ui

--- desktop/main.py
+++ desktop/main.py
@@ -1,18 +1,27 @@
 import sys
+from desktop.inretface import Ui_Form
+from PyQt5.QtWidgets import QApplication, QMainWindow
 
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
 
+class App(QMainWindow, Ui_Form):
+	def __init__(self):
+		super().__init__()
+		self.setupUi(self)
+		self.setup_ui()
 
-class App(QApplication, QMainWindow):
-    def __init__(self):
-        super().__init__()
+	def setup_ui(self):
+		self.username = "Anonymus"
 
-    def setup_ui(self):
-        pass
+	def login(self):
+		name = self.name.text()
+		password = self.password.text()
+		# далее отправка на api и получения результатов
+		# если ответ нормальный то мы меняем имя и теперь все отправки будут от этого имени
+		self.username = name
 
 
 if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    ex = App()
-    ex.show()
-    sys.exit(app.exec_())
+	app = QApplication(sys.argv)
+	ex = App()
+	ex.show()
+	sys.exit(app.exec_())
