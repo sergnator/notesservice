89e45dde0dc8881fcafba9ab78b64ce700057150
home1 <sergnator.de@gmail.com>
2024-03-26 22:15:32 +0300

    test v1.0

--- api/test.py
+++ api/test.py
@@ -1,8 +1,17 @@
-from requests import get, post
+from requests import get, post, put, delete
 
-print(post("http://127.0.0.1:5000/api/v2/users", json={"username": "1", "password": "123", "notes": [
-	{"content": "first not private", "private": False}, {"content": "second not private", "private": False},
-	{"content": "third private", "private": True}]}).json())  # create user
-print(get("http://127.0.0.1:5000/api/v2/users/1").json())  # get not private notes of user
-print(get("http://127.0.0.1:5000/api/v2/users",
-		json={"username": "1", "password": "123"}).json())  # get all notes of user
+print(get("http://127.0.0.1:5000/api/v2/notes/1").json())  # error not found
+
+
+print(get("http://127.0.0.1:5000/api/v2/users/random_username").json())  # not found
+
+print(post("http://127.0.0.1:5000/api/v2/users", json={"username": "test1", "password": "123456", "notes": [
+	{"content": "test_note_1", "private": False}, {"content": "test_note_2", "private": True}
+]}).json())  # create user
+
+print(get("http://127.0.0.1:5000/api/v2/notes/1").json())  # get note
+print(get("http://127.0.0.1:5000/api/v2/notes/2").json())  # not found because 2 is private
+
+print(get("http://127.0.0.1:5000/api/v2/users/test1").json())  # not found
+
+
