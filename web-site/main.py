aef698c861c93f664290582d2996f16a7b4a8055
home1 <sergnator.de@gmail.com>
2024-03-28 10:21:25 +0300

    login.html and registration.html

--- web-site/main.py
+++ web-site/main.py
@@ -3,10 +3,20 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+@app.route("/login")
+def login():
+    return render_template("login.html", title="Login")
+
+
+@app.route("/register")
+def registration():
+    return render_template("registration.html", title="Register")
+
+
 @app.route("/")
 @app.route("/index")
 def index():
-	return render_template("base.html")
+    return render_template("base.html", title="Home")
 
 
 app.run()
