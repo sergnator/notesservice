6bd9ac4faf2ca9d086db08fe71d56afc98836037
sergnator <86975413+sergnator@users.noreply.github.com>
2024-04-07 10:42:43 +0300

    writenote

--- web-site/forms.py
+++ web-site/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired
 
 
@@ -8,3 +8,9 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember me')
     submit = SubmitField('Login')
+
+
+class WriteNoteForm(FlaskForm):
+    is_private = BooleanField('Private')
+    content = TextAreaField('Content', validators=[DataRequired()])
+    submit = SubmitField('Write')
--- web-site/main.py
+++ web-site/main.py
@@ -1,8 +1,11 @@
-from flask import Flask, render_template, redirect
-from flask_login import LoginManager, current_user, logout_user
+from flask import Flask, render_template, redirect, url_for, request, make_response
+from flask_login import LoginManager, current_user, logout_user, login_required
 from flask_login import login_user as login_user_flask
+
 from api import *
-from forms import LoginForm
+from forms import LoginForm, WriteNoteForm
+
+import datetime
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mega_secret_key'
@@ -21,8 +24,13 @@ def login_():
     if form.validate_on_submit():
         user = login({"username": form.username.data, "password": form.password.data})
         if isinstance(user, User):
-            login_user_flask(user)
-            return redirect("/")
+            max_age = None
+            if form.remember_me.data:
+                max_age = datetime.timedelta(seconds=60 * 60 * 24 * 365)
+            login_user_flask(user, remember=form.remember_me.data, duration=max_age)
+            res = make_response(redirect(url_for('index')))
+            res.set_cookie("password", form.password.data, max_age=max_age)
+            return res
         return render_template("login.html", title="Login", form=form, error=user)
     return render_template("login.html", title="Login", form=form, error="None")
 
@@ -33,22 +41,52 @@ def registration():
     if form.validate_on_submit():
         user = register({"username": form.username.data, "password": form.password.data})
         if isinstance(user, User):
-            login_user_flask(user)
-            return redirect("/")
+            max_age = None
+            if form.remember_me.data:
+                max_age = datetime.timedelta(seconds=60 * 60 * 24 * 365)
+            login_user_flask(user, remember=form.remember_me.data, duration=max_age)
+            res = make_response(redirect(url_for('index')))
+            res.set_cookie("password", form.password.data, max_age=max_age)
+            return res
         return render_template("registration.html", title="Register", form=form, error=user)
     return render_template("registration.html", title="Register", error="None", form=form)
 
 
+@app.route("/create", methods=["GET", "POST"])
+@login_required
+def create():
+    form = WriteNoteForm()
+    if form.validate_on_submit():
+        note = {"content": form.content.data, "is_private": form.is_private.data}
+        user = User.from_dict({"username": current_user.username, "password": request.cookies.get("password")})
+        res = create_note(note, user)
+        if not isinstance(res, Note):
+            return render_template("create.html", title="Create Note", form=form, error=res)
+    return render_template("create.html", title="Create", form=form, error="None")
+
+
 @app.route("/logout")
+@login_required
 def logout_():
     logout_user()
-    return redirect("/")
+    res = make_response(redirect(url_for('index')))
+    res.set_cookie("password", "", max_age=0)
+    return res
 
 
 @app.route("/")
 @app.route("/index")
+@login_required
 def index():
     return render_template("base.html", title="Home", current_user=current_user)
+
+
+@app.after_request
+def redirect_to_sign(response):
+    if response.status_code == 401:
+        return redirect(url_for('login_'))
+
+    return response
 
 
 app.run(port=8000)
