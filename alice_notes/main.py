612b90b41b6e399b167517d7d3ed96e78bcd9703
home1 <sergnator.de@gmail.com>
2024-04-25 18:34:17 +0300

    read myself notes

--- alice_notes/main.py
+++ alice_notes/main.py
@@ -1,5 +1,6 @@
 from alice_yandex import *
 from api import *
+import ngrok
 
 buttons = ButtonList()
 buttons.add_button("написать", hide=True)
@@ -24,7 +25,7 @@ def register_token(request: RequestData):
     if not isinstance(res, list):
         return Response(f"Сервер выдал ошибку: {res}, повторите попытку позже", end_session=True)
     tokens[request.session["session_id"]] = token
-    return Response(f"Токен зарегистрирован")
+    return Response(f"Токен зарегистрирован", buttons=buttons)
 
 
 @alice.on_message
@@ -33,13 +34,15 @@ def on_message(request: RequestData):
 
 
 @alice.message("заметки")
-def notes(request: RequestData):
+def notes_(request: RequestData):
     if request.session["session_id"] not in tokens.keys():
         return Response("Вам нужен токен", end_session=True)
     token = tokens.get(request.session["session_id"])
     res: list[Note] = get_notes(token)
     if not isinstance(res, list):
         return Response(f"Сервер выдал ошибку: {res}, повторите попытку позже", end_session=True)
+    if len(res) == 0:
+        return Response("У вас пока что нет заметок", buttons=buttons)
     current_note = 0
     notes[request.session["session_id"]] = (res, current_note)
     buttons_for_note = ButtonList()
@@ -53,7 +56,7 @@ def notes(request: RequestData):
 
 def iter_notes(request: RequestData):
     notes_, current_note = notes[request.session["session_id"]]
-    if request.request.command == ">>":
+    if request.request.original_utterance == ">>":
         if len(notes_) < 1:
             buttons_for_note = ButtonList()
             buttons_for_note.add_button("отмена")
@@ -62,18 +65,31 @@ def iter_notes(request: RequestData):
             alice.register_next_step(iter_notes, request.session["session_id"])
             return Response("У вас нет больше записок", buttons=buttons_for_note)
         current_note += 1
-        alice.register_next_step(iter_notes, request.session["session_id"])
-        buttons_for_note = ButtonList()
-        buttons_for_note.add_button("отмена")
-        if 0 != current_note:
-            buttons_for_note.add_button("<<")
-        if current_note != len(notes_) - 1:
-            buttons_for_note.add_button(">>")
-        alice.register_next_step(iter_notes, request.session['session_id'])
-        notes[request.session["session_id"]] = (notes_, current_note)
-        return Response(
-            f"Id: {notes_[current_note].id}\n{notes_[current_note].content}\nприватность:{notes_[current_note].private}",
-            buttons=buttons_for_note)
+
+    if request.request.original_utterance == "<<":
+        if current_note == 0:
+            buttons_for_note = ButtonList()
+            buttons_for_note.add_button("отмена")
+            if current_note == len(notes) - 1:
+                buttons_for_note.add_button(">>")
+            return Response("Это первая заметка")
+        current_note -= 1
+
+    if request.request.original_utterance == "отмена":
+        del notes[request.session["session_id"]]
+        return Response("отменяю", buttons=buttons)
+
+    buttons_for_note = ButtonList()
+    buttons_for_note.add_button("отмена")
+    if current_note != 0:
+        buttons_for_note.add_button("<<")
+    if current_note != len(notes) - 1:
+        buttons_for_note.add_button(">>")
+    alice.register_next_step(iter_notes, request.session['session_id'])
+    notes[request.session["session_id"]] = (notes_, current_note)
+    return Response(
+        f"Id: {notes_[current_note].id}\n{notes_[current_note].content}\nприватность:{notes_[current_note].private}",
+        buttons=buttons_for_note)
 
 
 alice.run(port="1000")
--- alice_notes/з.py
+++ alice_notes/з.py
@@ -1,4 +1,9 @@
-import requests
+import ngrok
 
+ngrok.set_auth_token("2fb5IKJduHgYmK6HV9S2tz2atGo_A3DBmEsa21eghzektiru")
+listener = ngrok.forward(1000)
 
-response = requests.post('http://127.0.0.1:8888/post')
+print(f"Ingress established at {listener.url()}")
+print("press Ctrl+C to exit")
+while True:
+    pass
