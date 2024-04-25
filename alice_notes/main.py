50f1127ebc99befda0e9da3b79b8f5628ca91a1b
home1 <sergnator.de@gmail.com>
2024-04-25 17:47:31 +0300

    alice messages

--- alice_notes/main.py
+++ alice_notes/main.py
@@ -8,6 +8,9 @@ buttons.add_button("заметки", hide=True)
 
 alice = Alice(__name__)
 
+tokens = {}
+notes = {}
+
 
 @alice.on_start
 def on_start(request: RequestData):
@@ -19,12 +22,9 @@ def register_token(request: RequestData):
     token = request.request.original_utterance
     res = get_notes(token)
     if not isinstance(res, list):
-        return Response(f"Сервер выдал ошибку: {res}, повторите попытку")
-    res_str = "\n".join(
-        [f"ID: {note.id}\nТекст:\n{note.content}\nПриватная:{note.private}\n----------------------------------\n" for
-         note in res])
-    print(res_str)
-    return Response(res_str)
+        return Response(f"Сервер выдал ошибку: {res}, повторите попытку позже", end_session=True)
+    tokens[request.session["session_id"]] = token
+    return Response(f"Токен зарегистрирован")
 
 
 @alice.on_message
@@ -32,4 +32,48 @@ def on_message(request: RequestData):
     return Response(f"{request.request.original_utterance}")
 
 
-alice.run(port="8888")
+@alice.message("заметки")
+def notes(request: RequestData):
+    if request.session["session_id"] not in tokens.keys():
+        return Response("Вам нужен токен", end_session=True)
+    token = tokens.get(request.session["session_id"])
+    res: list[Note] = get_notes(token)
+    if not isinstance(res, list):
+        return Response(f"Сервер выдал ошибку: {res}, повторите попытку позже", end_session=True)
+    current_note = 0
+    notes[request.session["session_id"]] = (res, current_note)
+    buttons_for_note = ButtonList()
+    buttons_for_note.add_button("отмена")
+    if len(res) > 1:
+        buttons_for_note.add_button(">>")
+    alice.register_next_step(iter_notes, request.session["session_id"])
+    return Response(f"Id: {res[current_note].id}\n{res[current_note].content}\nприватность:{res[current_note].private}",
+                    buttons=buttons_for_note)
+
+
+def iter_notes(request: RequestData):
+    notes_, current_note = notes[request.session["session_id"]]
+    if request.request.command == ">>":
+        if len(notes_) < 1:
+            buttons_for_note = ButtonList()
+            buttons_for_note.add_button("отмена")
+            if 0 != current_note:
+                buttons_for_note.add_button("<<")
+            alice.register_next_step(iter_notes, request.session["session_id"])
+            return Response("У вас нет больше записок", buttons=buttons_for_note)
+        current_note += 1
+        alice.register_next_step(iter_notes, request.session["session_id"])
+        buttons_for_note = ButtonList()
+        buttons_for_note.add_button("отмена")
+        if 0 != current_note:
+            buttons_for_note.add_button("<<")
+        if current_note != len(notes_) - 1:
+            buttons_for_note.add_button(">>")
+        alice.register_next_step(iter_notes, request.session['session_id'])
+        notes[request.session["session_id"]] = (notes_, current_note)
+        return Response(
+            f"Id: {notes_[current_note].id}\n{notes_[current_note].content}\nприватность:{notes_[current_note].private}",
+            buttons=buttons_for_note)
+
+
+alice.run(port="1000")
